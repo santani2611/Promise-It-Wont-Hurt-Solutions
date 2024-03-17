cc738b79fd1c8c084165d01e5ec4c58fc825fffe
santani2611 <santaniabhimanyu@gmail.com>
2024-03-17 05:38:59 +0530

    8/13

--- promise-shop/program.js
+++ promise-shop/program.js
@@ -83,7 +83,7 @@
 
 //abhi tak tumhare handlers then catch bas print kar rahi thi values /errors, kya karoge agar then/catch ko kuch asynchronous karna ho.
 
-first().then(x=>second(x).then(x=>console.log(x)));
+//first().then(x=>second(x).then(x=>console.log(x)));
 
 //second().then(x=>x)
 
@@ -97,3 +97,31 @@ first().then(x=>second(x).then(x=>console.log(x)));
 // })
 
 // console.log(promise.then(x=>console.log(x)));
+
+//excercise 8/13
+
+//do i have to return promises?
+
+//.then handle karega ye sab, promise ki library wrap kar degi return value in a promise agar jarurat hogi.
+
+// ye mast batt hai kyunki tumhe ghanta nhi matlab prmise . then de rsha ya value toh tum mast .then().then ki chain banao bina ye soche ki kon value de raha kon promise.
+
+// The key thing to understand here is that your handlers will wrap your
+// return values in promises even if they are obtained synchronously.
+
+// Another very important point to understand is that, as discussed before, the
+// returned value will resolve on the next turn of the event loop.
+
+// ## Task
+
+// Construct a promise chain that returns values to prove to yourself that
+// promise handlers will wrap your returned values in promises allowing
+// additional chaining.
+
+function attachTitle(x){
+  return `DR. ${x}`
+}
+Promise.resolve('MANHATTAN')
+.then(attachTitle)
+.then(console.log);
+
